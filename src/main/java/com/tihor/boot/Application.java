78efcd0ffa3f700efebf99a1d4bcfee3f7a4a34c
Ksheerasagar, RohithKumar <RohithKumar.Ksheerasagar@adp.com>
2017-04-20 16:25:02 +0530

    package as an executable jar.

---
+++ src/main/java/com/tihor/boot/Application.java
@@ -0,0 +1,14 @@
+package com.tihor.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
